7b86c284f202985ae456dff30bee9f164167a89f
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-04 16:58:09 -0600

    Pequena modificacion

--- Ejercicio1_practica1.py
+++ Ejercicio1_practica1.py
@@ -2,8 +2,8 @@
 
 print('Hola mundo')
 
-num1 = int(input('Escribe un numero del uno al 10'))
-num2 = int(input('Escribe otro numero del 1 al 10'))
+num1 = int(input('Escribe un numero del uno al 10 '))
+num2 = int(input('Escribe otro numero del 1 al 10 '))
 
 suma = num1 + num2
 resta = num1 - num2
@@ -14,4 +14,6 @@ print(f'''El resultado de la suma es {suma}
 El resultado de la resta es {resta}.
 El resultado de la multiplicacion es {multiplicacion}.
 El resultado de la division es {division} ''')
+
+print(f"El resultado de la suma es {suma}")
 
