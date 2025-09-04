88838383b3c49d3823538c86b92ec824fb26cf24
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-04 17:01:07 -0600

    creacion de la clase carro

---
+++ practicas2.py
@@ -0,0 +1,11 @@
+# Clases y objetos.
+
+class carro:
+
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+
+        
+        
